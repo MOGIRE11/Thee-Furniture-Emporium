aa52a7e125930254eb6a687ca71e85682d66a3e6
Your Name <mmogire11@gmail.com>
2024-04-03 20:27:45 +0300

    deleted redundant js scripts

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-//const filterInput=document.getElementById('search');
-// const listItems=document.querySelectorAll('#myUl li');
-//const listItems = Array.from(document.querySelectorAll("#myUl li"));
+const filterInput=document.getElementById('search');
+//const listItems=document.querySelectorAll('#myUl li');
+const listItems = Array.from(document.querySelectorAll("#myUl li"));
 
-// filterInput.addEventListener('input',function()
-// {
-//     const filterValue=filterInput.value.toLowerCase();
+filterInput.addEventListener('input',function()
+{
+    const filterValue=filterInput.value.toLowerCase();
     
-//     listItems.forEach(item=>
-//         {
-//             const text=item.textContent.toLowerCase();
-//             item.style.display=text.includes(filterValue)?
-//             'block':'none';
-//         });
+    listItems.forEach(item=>
+        {
+            const text=item.textContent.toLowerCase();
+            item.style.display=text.includes(filterValue)?
+            'block':'none';
+        });
     
-// });
-
-
-// const searchItem=document.querySelector('.navsearch');
-// const searchInput=document.querySelector('#search');
-// searchItem.addEventListener('click', function(){
-//     searchItem.style.width='250px';
-//     searchInput.style.display='flex';
-//     searchInput.style.width='85%';
-// });
-
+});
 
 // hamburger menu
 const burger=document.querySelector('.listMenu');
