657669f51060ec578f52fef1e5e58c816e2f9051
Your Name <mmogire11@gmail.com>
2025-04-12 22:50:06 +0300

    ....

--- app.js
+++ app.js
@@ -1,44 +1,166 @@
-const filterInput=document.getElementById('search');
-//const listItems=document.querySelectorAll('#myUl li');
-const listItems = Array.from(document.querySelectorAll("#myUl li"));
-
-filterInput.addEventListener('input',function()
-{
-    const filterValue=filterInput.value.toLowerCase();
-    
-    listItems.forEach(item=>
-        {
-            const text=item.textContent.toLowerCase();
-            item.style.display=text.includes(filterValue)?
-            'block':'none';
-        });
-    
-});
-
-// hamburger menu
-const burger=document.querySelector('.listMenu');
-const ourList=document.querySelector('.nav-list');
-const exitList=document.querySelector('.cross')
-
-burger.addEventListener('click',function(){
-    ourList.classList.toggle('open-list')
-});
-
-exitList.addEventListener('click',function(){
-    ourList.classList.remove('open-list');
-});
-
-const chevron=document.querySelector('.chevron');
-chevron.onclick=()=>{
-    chevron.classList.toggle('down');
-    productList.classList.toggle('show-more-products');
-};
-
-const productList=document.querySelector('.more-products');
-productList.addEventListener('click',function(){
-    productList.classList.toggle('show-more-products');
-});
-
-
-
-
+{/* <div class="page1 bg-red-50">
+            <section class="left" data-aos="fade-right"
+            data-aos-duration="1000"  data-aos-anchor-placement="center-bottom">
+               
+                   <h2>Transform your <br><span class="animate"> </span></h2> <br>
+                    <!-- office,bedroom,commercial space,living room,dining room -->
+                   <p class="description"> 
+                    Create a sanctuary of comfort and elegance with our premium furniture collection.
+                    Our stylish and functional pieces are crafted with care to meet your unique style and needs.
+                    From cozy beds to chic sofas, we offer a curated selection of furniture that will make your home or business stand out.
+                    Shop now and let us help you bring your design vision to life!
+                </p><br>
+                
+                <p class="hidden_paragraph"">
+                    Create a sanctuary of comfort and elegance with our premium furniture collection.We offer a curated selection of furniture shop now and let us help you bring your design vision to life!
+                </p><br>
+                
+                <button id="catalogue">
+                    <a href="products.html">VIEW OUR CATALOGUE</a>
+                    
+                </button>
+            </section>
+            
+            <section class="right" >
+                <img src="istock/living_room.png" id="sofa3" loading="lazy" alt="sofa">
+                <img src="istock/sale.png" alt="on sale promo" id="promo" data-aos="fade-down" loading="lazy">
+            </section>
+            
+        </div>
+        
+        <div class="page2">
+        <h2>EXPLORE OUR FURNITURE <span>RANGE</span></h2> 
+        <div class="card">
+            <section ><span id="pp1"><p>SEATS</p></span></section>
+            <section ><span id="pp2"><p>SOFAS</p></span></section>
+            <section ><span id="pp3"><p>BEDS</p></span></section>
+            <section ><span id="pp4"><p>CABINETS</p></span></section>
+            <section ><span id="pp5"><p>OFFICE</p></span></section>
+            <section ><span id="pp6"><p>DINING</p></span></section>
+            <section ><span id="pp7"><p>WARDROBES</p> </span></section>
+        </div>  
+        </div>
+              
+        <div class="page3">
+            <!-- services -->
+            <section class="stop">
+                <h2>Our Services</h2>
+                <section id="sp">         
+                    <p data-aos="fade-right"
+                    data-aos-duration="1000">We offer a vast selection of furniture items that cater to all budgets. We understand that furniture is a big investment, which is why we only stock the highest quality products that are built to last.</p>
+                    
+                    <p data-aos="fade-left"
+                    data-aos-duration="1000">We provide you with exceptional customer service and assistance in finding the perfect furniture pieces to fit your style and budget.</p>
+                    
+                
+                </section>
+               </section>
+               
+               
+            <section class="sbottom">
+                <div class="service1">
+                    <img src="istock/quality_product.png" alt="custom product">
+                    <h2>Custom Products</h2>
+                        <p>
+                            We ensure that each piece is not only unique but also durable. You can trust that your piece will withstand the test of time. </p>
+                </div>
+                <div class="service2">
+                    <img src="istock/delivery_van.png" id="deliver" alt="delivery van">
+                    <h2>Fast delivery</h2>
+                        <p>
+                            Expect your order to arrive quickly and in perfect condition without having to worry about any extra charges.  
+                        </p>
+                </div>
+                <div class="service3">
+                    <img src="istock/transaction.png" alt="business transaction">
+                    <h2>Payment method</h2>
+                        <p>
+                            Easy and hassle-free process.Complete your transaction securely and quickly with a few clicks.
+                        </p>
+                </div>
+                <div class="floss">
+                    <section>
+                        <h2>10+</h2>
+                        <p>Years of experience</p>
+                    </section>
+                    <section>
+                        <h2>2500+</h2>
+                        <p>Worldwide Customers</p>
+                    </section>
+                    <section>
+                        <h2>3000+</h2>
+                        <p>Completed orders</p>
+                    </section>
+                </div>
+                
+               
+                <!-- <div class="partners">
+                    <h2></h2>
+                    <section>
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">                       
+                    </section>
+                </div> -->
+            </section>
+        </div>
+        
+        <div class="footer">
+            <h2 class="testh2">TESTIMONIALS</h2>
+            <section class="testimony">
+                <!-- testimonials -->
+                <div class="test1">
+                    <p>
+                        "I'm absolutely thrilled with the furniture I bought from this store! The quality is outstanding and the prices are very reasonable. I highly recommend this store to anyone in need of stylish and affordable furniture."
+                    </p>
+                    <section>
+                        <img src="istock/man_headshot.jpg" alt="man_headshot">
+                        <h3>
+                            Camello Anthony<br>
+                            <span>Ontario ,Argentina. </span>
+                        </h3>
+                    </section>
+                </div>
+                <div class="test2">
+                    <p>
+                        "I found the perfect coffee table at this furniture store! The staff was friendly and knowledgeable and helped me find exactly what I was looking for. I will definitely be returning for any future furniture needs."
+                   </p>
+                    <section>
+                        <img src="istock/muslim_headshot.jpg" alt="chinese_headshot">
+                        <h3>
+                            Monica Juma <br>
+                            <span>Karachi ,Pakistan. </span>
+                        </h3>
+                    </section>
+                </div>
+                <div class="test3">
+                    <p>
+                        "The store has a fantastic selection of furniture that is both stylish and functional. The delivery process was quick and easy, and the furniture was in perfect condition upon arrival. I highly recommend this store!"
+                    </p>
+                    <section>
+                        <img src="istock/chinese_headshot.jpg" alt="">
+                        <h3>
+                            Jeremy Lee <br>
+                            <span>Hong kong, China.</span>
+                        </h3>
+                    </section>
+                </div>
+                <div class="test4">
+                    <p>
+                        "I had a great experience shopping at this furniture store. The staff was attentive and helpful, and the furniture selection was top-notch. I found exactly what I was looking for and couldn't be happier with my purchase."
+                    </p>
+                    <section>
+                        <img src="istock/woman_headshot.png" alt="">
+                        <h3>
+                            Naomi Theuri<br>
+                            <span>Nairobi, Kenya. </span>
+                        </h3>
+                    </section>
+                </div>
+            </section>
+            
+            
+            <main-footer></main-footer>        
+        </div> */}
